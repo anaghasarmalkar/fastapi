ca4c13ff1a6ccc1d4ba6f3ff2640617aa4abcb60
Anagha Sarmalkar <anaghasarmalkar@gmail.com>
2024-07-06 15:53:00 -0600

    Use factory pattern for app

--- app/main.py
+++ app/main.py
@@ -1,37 +1,38 @@
 import uvicorn
-
 from fastapi import FastAPI
-
-from app.db.database import engine
-from app.models import user as user_model, room as room_model, room_user as room_user_model
-from app.routers import user, auth
-
 from fastapi.middleware.cors import CORSMiddleware
-
-user_model.Base.metadata.create_all(bind=engine)
-room_model.Base.metadata.create_all(bind=engine)
-room_user_model.Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-app.include_router(auth.router)
-app.include_router(user.router)
+from app.db.database import init_db
+from app.routers import user, auth, room
 
 
-origins = [
-    "http://localhost:3000",
-    "http://0.0.0.0:3000",
-    "http://127.0.0.1:3000"
-]
+def main():
+    init_db()
+    # seed_data()
 
-# A "middleware" is a function that works with every request before it is processed by any specific path operation. And also with every response before returning it.
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    app = FastAPI()
+    app.include_router(auth.router)
+    app.include_router(user.router)
+    app.include_router(room.router)
 
+    origins = [
+        "http://localhost:3000",
+        "http://0.0.0.0:3000",
+        "http://127.0.0.1:3000"
+    ]
+
+    # A "middleware" is a function that works with every request before it is processed by any specific path operation. And also with every response before returning it.
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return app
+
+
+app = main()
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8081, reload=True)
